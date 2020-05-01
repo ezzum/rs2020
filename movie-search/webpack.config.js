084a49765d1b12479a6f96244dce49ec618016d6
allihach <lihach.ac@gmail.com>
2020-05-02 00:17:01 +0400

    feat: edit config

--- movie-search/webpack.config.js
+++ movie-search/webpack.config.js
@@ -24,9 +24,9 @@ module.exports = (env, options) => {
                     exclude: /node_modules/,
                     use: {
                       loader: 'babel-loader',
-                      options: {
-                        presets: ['@babel/preset-env']
-                      }
+                    //   options: {
+                    //     presets: ['@babel/preset-env']
+                    //   }
                     }
                 },
                 {
