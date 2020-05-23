c40b1028151fcaf3e33216f5188d5090eab8c9b3
allihach <lihach.ac@gmail.com>
2020-05-24 01:23:59 +0400

    feat: add get image

---
+++ fancy-weather/src/background.js
@@ -0,0 +1,20 @@
+export default class Background {
+  constructor() {
+    this.key = 'o2h5xaDXAumXpc4rNhG7WjMgWqKqLH2w3TQhgC7LxVc';
+  }
+
+  getImage() {
+    const weatherCode = JSON.parse(sessionStorage.getItem('weatherNow')).code;
+    const heightImg = window.innerHeight;
+    const url = `https://api.unsplash.com/photos/random?orientation=landscape&per_page=1&query=weather-${weatherCode}&client_id=${this.key}`;
+    const event = new Event('load');
+
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        sessionStorage.setItem('image', `${data.urls.raw}&h=${heightImg}`);
+
+        document.querySelector('body').dispatchEvent(event);
+      });
+  }
+}
